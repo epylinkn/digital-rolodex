4221c376cd50fd970cf252b99c41635ea54830f8
Anthony Bui <anthonybui@gmail.com>
2019-04-08 15:27:38 -0400

    Add some fake buttons in a 3-tool layout

--- src/routes/Contacts/components/NewContactTile/NewContactTile.js
+++ src/routes/Contacts/components/NewContactTile/NewContactTile.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ContentAddCircle from '@material-ui/icons/AddCircle'
+import ContentCalendarToday from '@material-ui/icons/CalendarToday'
+import ContentCake from '@material-ui/icons/Cake'
 import Paper from '@material-ui/core/Paper'
 
-const iconSize = '6rem'
+const iconSize = '2rem'
 const iconStyle = { width: iconSize, height: iconSize }
 
 function NewContactTile({ onClick, classes }) {
   return (
-    <Paper className={classes.root} onClick={onClick}>
-      <ContentAddCircle style={iconStyle} />
-    </Paper>
+    <div>
+      <div className={classes.newButtons}>
+        <Paper className={classes.root} onClick={onClick}>
+          <ContentAddCircle style={iconStyle} />
+          <div className={classes.iconLabel}>New Contact</div>
+        </Paper>
+      </div>
+      <div className={classes.newButtons}>
+        <Paper className={classes.root}>
+          <ContentCalendarToday style={iconStyle} />
+          <div className={classes.iconLabel}>Office Hours</div>
+        </Paper>
+      </div>
+      <div className={classes.newButtons}>
+        <Paper className={classes.root}>
+          <ContentCake style={iconStyle} />
+          <div className={classes.iconLabel}>Cake Days</div>
+        </Paper>
+      </div>
+    </div>
   )
 }
 
--- src/routes/Contacts/components/NewContactTile/NewContactTile.styles.js
+++ src/routes/Contacts/components/NewContactTile/NewContactTile.styles.js
@@ -1,12 +1,19 @@
 export default theme => ({
   root: {
-    ...theme.flexRowCenter,
+    ...theme.flexColumnCenter,
     alignItems: 'center',
+    justifyContent: 'center',
     cursor: 'pointer',
-    height: '200px',
+    height: '100px',
     width: '300px',
     margin: theme.spacing.unit * 0.5,
     padding: theme.spacing.unit * 1.3,
     overflow: 'hidden'
+  },
+  newButtons: {
+    display: 'inline-block'
+  },
+  iconLabel: {
+    marginTop: '10px'
   }
 })
